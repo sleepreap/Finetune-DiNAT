35d77ab9d0eb39004be5b05346129fa28dac48b5
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-05-23 15:28:55 +0800

    Create train.py

---
+++ scripts/train.py
@@ -0,0 +1,33 @@
+import pytorch_lightning as pl
+import torch
+torch.manual_seed(1)
+torch.set_float32_matmul_precision("medium")
+from mask2former import ( Mask2FormerFinetuner, 
+                        SegmentationDataModule, 
+                        DATASET_DIR, 
+                        BATCH_SIZE, 
+                        NUM_WORKERS, 
+                        ID2LABEL, 
+                        LEARNING_RATE, 
+                        LOGGER,  
+                        DEVICES,
+                        CHECKPOINT_CALLBACK, 
+                        EPOCHS )
+
+
+if __name__=="__main__":
+    data_module = SegmentationDataModule(dataset_dir=DATASET_DIR, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+    model=Mask2FormerFinetuner(ID2LABEL, LEARNING_RATE)
+
+    trainer = pl.Trainer(
+        logger=LOGGER,
+        accelerator='cuda',
+        devices=DEVICES,
+        strategy="ddp",
+        callbacks=[CHECKPOINT_CALLBACK],
+        max_epochs=EPOCHS
+    )
+    print("Training starts!!")
+    trainer.fit(model,data_module)
+    print("saving model!")
+    trainer.save_checkpoint("dinat.ckpt")
