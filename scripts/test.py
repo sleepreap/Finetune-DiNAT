581a943a9264e33d233855772157d53978f48a58
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-05-25 16:44:02 +0800

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -9,8 +9,7 @@ from DiNAT import ( Mask2FormerFinetuner,
                         NUM_WORKERS, 
                         ID2LABEL, 
                         LEARNING_RATE, 
-                        LOGGER, 
-                        PRECISION)
+                        LOGGER)
 import argparse
 
 
@@ -30,7 +29,6 @@ if __name__=="__main__":
 
     trainer = pl.Trainer(
         logger=LOGGER,
-        precision=PRECISION,
         accelerator='cuda',
         devices=[0],
         num_nodes=1,
